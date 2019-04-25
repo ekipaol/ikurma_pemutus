3c64f4088a9f46292c42f56401ff56c6124c3e49
Ridho Pratama <ridho.pratama@mail.ugm.ac.id>
2019-04-25 07:28:04 +0700

    [Update] Fix Home

--- app/src/main/java/com/application/bris/brisi/MainActivity.java
+++ app/src/main/java/com/application/bris/brisi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.application.bris.brisi;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
@@ -8,17 +9,19 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-//    @BindView(R.id.appbar)
-//    AppBarLayout appbar;
-//    @BindView(R.id.collapsing_toolbar)
-//    CollapsingToolbarLayout collapsing_toolbar;
-//    @BindView(R.id.toolbar)
-//    Toolbar toolbar;
+    @BindView(R.id.appbar)
+    AppBarLayout appbar;
+    @BindView(R.id.collapsing_toolbar)
+    CollapsingToolbarLayout collapsing_toolbar;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
     @BindView(R.id.navigation)
     BottomNavigationView navigation;
 
@@ -42,10 +45,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         ButterKnife.bind(this);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-//        setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
 //        collapsing_toolbar.setCollapsedTitleTextColor(getApplicationContext().getResources().getColor(R.color.colorWhite));
 //        collapsing_toolbar.setExpandedTitleColor(getApplicationContext().getResources().getColor(R.color.colorPrimary));
 
