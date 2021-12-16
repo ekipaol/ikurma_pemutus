bc128950944cd9df0896e652e22263ec2990ace4
Eki Iskandar <eki_iskandar@yahoo.com>
2021-12-16 16:30:59 +0700

    -merged pertashop dan eklaim di hide

--- app/src/main/java/com/application/bris/brisi_pemutus/config/menu/Menu.java
+++ app/src/main/java/com/application/bris/brisi_pemutus/config/menu/Menu.java
@@ -46,7 +46,7 @@ public class Menu {
         if(!appPreferences.getFidRole().equalsIgnoreCase("71")&&!appPreferences.getFidRole().equalsIgnoreCase("77")){
             menu.add(new ListViewMenu(R.drawable.ic_ambil_alih_2, "Ambil Alih Putusan"));
             //MENU EKLAIM
-            menu.add(new ListViewMenu(R.drawable.ic_eklaim, "e-Klaim"));
+//            menu.add(new ListViewMenu(R.drawable.ic_eklaim, "e-Klaim"));
         }
 
         //halaman ao silang hanya bisa diakses pinca pincapem MM
@@ -145,7 +145,7 @@ public class Menu {
 
 
         //MENU EKLAIM
-        menu.add(new ListViewMenu(R.drawable.ic_eklaim, "e-Klaim"));
+//        menu.add(new ListViewMenu(R.drawable.ic_eklaim, "e-Klaim"));
 
 
 
@@ -188,7 +188,7 @@ public class Menu {
         menu.add(new ListViewMenu(R.drawable.ic_daftar_putusan, context.getString(R.string.menu_pengajuan)));
         menu.add(new ListViewMenu(R.drawable.ic_riwayat, "Riwayat"));
         menu.add(new ListViewMenu(R.drawable.ic_business,"Monitoring"));
-        menu.add(new ListViewMenu(R.drawable.ic_eklaim, "e-Klaim"));
+//        menu.add(new ListViewMenu(R.drawable.ic_eklaim, "e-Klaim"));
         menu.add(new ListViewMenu(R.drawable.ic_logout_front, "Logout"));
 
 
@@ -198,7 +198,7 @@ public class Menu {
         menu.add(new ListViewMenu(R.drawable.ic_daftar_putusan, context.getString(R.string.menu_pengajuan)));
         menu.add(new ListViewMenu(R.drawable.ic_riwayat, "Riwayat"));
         menu.add(new ListViewMenu(R.drawable.ic_business,"Monitoring"));
-        menu.add(new ListViewMenu(R.drawable.ic_eklaim, "e-Klaim"));
+//        menu.add(new ListViewMenu(R.drawable.ic_eklaim, "e-Klaim"));
         menu.add(new ListViewMenu(R.drawable.ic_logout_front, "Logout"));
 
 
